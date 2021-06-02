746e46b8f08e54332047f7a3a91541a14c6c5f57
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-02 09:59:54 +0900

    Add files via upload

---
+++ cubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeController : MonoBehaviour
+{
+    float vert;
+    float horiz;
+    Transform cube_tr;
+    Rigidbody cube_rb;
+    public GameObject sphere;
+    void Start()
+    {
+        cube_tr = GetComponent<Transform>();
+        cube_rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        vert = Input.GetAxis("Vertical") * 10f;
+        horiz = Input.GetAxis("Horizontal") * 1f;
+        cube_rb.AddForce(cube_tr.forward * vert);
+        cube_tr.Rotate(0,horiz,0);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "enemy")
+        {
+            Destroy(sphere.gameObject);
+            print("сфера сломана");
+        }
+    }
+}
